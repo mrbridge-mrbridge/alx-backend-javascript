b3f1f0da552c30976758ae8db49d8a70d79fc244
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-05 16:47:01 +0000

    elint

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,5 +1,6 @@
 import getBudgetObject from './7-getBudgetObject';
 
+
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
