953645c61216a11d36690ea1be6e4f94eb732be4
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-05 14:35:02 +0000

    elint

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: {...employeesList},
+    allEmployees: {employeesList},
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     }
