b2fa136d78c3b3e9f91a8ac08193cca4d4ed93cf
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 22:22:18 +0000

    correct

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,15 +3,11 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
-  }
-
-  set floors(floors) {
-    this._floors = floors;
   }
 
   evacuationWarningMessage() {
