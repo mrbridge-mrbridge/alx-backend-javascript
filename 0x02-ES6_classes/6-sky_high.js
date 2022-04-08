a9deb2fb792222d130f420c4ad359ce36adc1c98
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 21:53:55 +0000

    method update

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -15,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
