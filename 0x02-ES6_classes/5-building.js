4cc426a5b2df2a72ec3cdf66e913c176c31f981b
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 21:36:04 +0000

    elint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    //if (new.target === Building) {
-    // throw new Error('Abstract class cannot be Instantiated');
-    //}
-
     if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
