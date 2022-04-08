a0d7e54514143fc29429fbe8edcc5cd5154a8555
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 02:33:45 +0000

    conversionrate

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(Amount) {
+    this._amount = Amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(Currency) {
+    this._currency = Currency;
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency._name} (${this._currency._code})`);
+  }
+
+  static displayFullPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
