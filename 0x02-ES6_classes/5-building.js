034dcd445b1405464a57d82aa6c0227262967a86
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 22:45:44 +0000

    evacuate method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,6 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+}
