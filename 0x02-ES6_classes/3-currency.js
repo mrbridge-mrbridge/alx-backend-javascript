5d15d30e7a1b544fa4aa199afe09fb3efe688f78
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-08 01:57:59 +0000

    elint check

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,23 +3,18 @@ export default class Currency{
     this.code = code;
     this.name = name;
   }
-
   get code() {
     return this._code;
   }
-
   set code(Code) {
     this._code = Code;
   }
-
   get name() {
     return this._name;
   }
-
   set name(Name) {
     this._name = Name;
   }
-
   displayFullCurrency() {
     return (`${this._name} (${this._code})`);
   }
