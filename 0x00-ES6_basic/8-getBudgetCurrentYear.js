0e5f8c97ea54e05576ef25b7bba0aff3893b05b7
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-09 02:22:38 +0000

    elint correct

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,12 +3,12 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita
   };
-
   return budget;
 }
