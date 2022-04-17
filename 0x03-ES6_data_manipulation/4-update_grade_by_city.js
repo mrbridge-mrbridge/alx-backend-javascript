7e7f5a16681d97f7e7c824c68724c85b46878775
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-17 13:55:56 +0000

    filter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studByLoc = students.filter((student) => student.location === city);
-  const studGrades = [];
-  studByLoc.map((student) => {
-    newGrades.map((item) => {
-      if (student.id === item.studentId) {
-        student.grade = item.grade;
-      }
-    });
-    if (!student.grade) {
-      student.grade = 'N/A';
-    }
-    studGrades.push(student);
-  });
-  return studGrades;
+  return students.filter((student) => student.location === city)
+    .map((student) => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: (newGrades.filter((item) => item.studentId === student.id).shift() || {grade:'N/A'}).grade
+    }));
 }
