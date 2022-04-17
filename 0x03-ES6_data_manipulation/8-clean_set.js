e9a01b24cf480e532108320dba1527b2b23ef288
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-17 18:27:00 +0000

    undefined item

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const newArray = [];
   if (!startString || startString.length === 0) return '';
   for (const item of set) {
-    if (typeof(item) === 'undefined') newArray.push('');
+    if (typeof (item) === 'undefined') continue;
     else if (item.startsWith(startString)) {
       newArray.push(item.replace(startString, ''));
     }
--- 0x03-ES6_data_manipulation/8-main.js
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -1,3 +1,3 @@
 import cleanSet from './8-clean_set.js';
 
-console.log(cleanSet(new Set(['id-test', 'id-chicken', ,'id-user', 'id-id-'])));
+console.log(cleanSet(new Set(['id-test', 'id-chicken', 'id-user', , 'id-id-']), 'id-'));
