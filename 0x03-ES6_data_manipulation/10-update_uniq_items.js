d7d1b1e54f44cdd97dbafb0dc48917d59906055b
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-17 19:31:43 +0000

    map.set

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,12 @@
-export default function updateUniqueItems() {}
+export default function updateUniqueItems(mapItem) {
+  if (!(mapItem instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (let [key, value] of mapItem) {
+    if (value === 1) {
+      mapItem.set(key, 100);
+    }
+  }
+  return mapItem;
+}
