bbb7fc747ff59621cbcd4cea4136463130c47362
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-17 18:33:40 +0000

    eslint no-continue

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,7 @@ export default function cleanSet(set, startString) {
   const newArray = [];
   if (!startString || startString.length === 0) return '';
   for (const item of set) {
-    if (typeof (item) === 'undefined') continue;
-    else if (item.startsWith(startString)) {
+    if (typeof (item) !== 'undefined' && item.startsWith(startString)) {
       newArray.push(item.replace(startString, ''));
     }
   }
