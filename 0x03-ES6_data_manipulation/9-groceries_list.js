583dfb365defb2f633d087b43302a3ea9b29670b
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-17 19:07:48 +0000

    eslint

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,4 @@
 export default function groceriesList() {
-  const groceries = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5],];
+  const groceries = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   return new Map(groceries);
 }
