c55a65e56f68224d56e7ce686c6b79e738edcf1c
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-21 10:21:41 +0000

    student class interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,27 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
     return firstName.charAt(0) + '. ' + lastName;
 }
 
+export interface IStudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor (firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
