6e7be00fe6a67bac3c078b840f99e35989b4489c
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-04-21 21:00:53 +0000

    crud files

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,70 +1,18 @@
-interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
-}
+/// <reference path="./crud.d.ts" />
 
-interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-}
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-export class Director implements DirectorInterface {
-    workFromHome(): string {
-        return 'Working from home';
-    }
+const row: RowElement = {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+};
 
-    getCoffeeBreak(): string {
-        return 'Getting a coffee break';
-    }
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {
+    ...row,
+    age: 23,
+};
 
-    workDirectorTasks(): string {
-        return 'Getting to director tasks';
-    }
-}
-
-export class Teacher implements TeacherInterface {
-    workFromHome(): string {
-        return 'Cannot work from home';
-    }
-
-    getCoffeeBreak(): string {
-        return 'Cannot have a break';
-    }
-
-    workTeacherTasks(): string {
-        return 'Getting to work';
-    }
-}
-
-export function createEmployee(salary: string | number): Teacher | Director {
-    if (typeof (salary) === 'number' && salary < 500) {
-        return new Teacher();
-    } else {
-        return new Director();
-    }
-}
-
-export function isDirector(employee: Director | Teacher): boolean {
-    return employee instanceof Director;
-}
-
-export function executeWork(employee: Director | Teacher): string {
-    if (isDirector(employee)) {
-        return (employee as Director).workDirectorTasks()
-    } else {
-        return (employee as Teacher).workTeacherTasks()
-    }
-}
-
-type Subjects = 'Math' | 'History';
-
-export function teachClass(todayClass: Subjects): string {
-    if (todayClass === 'Math') {
-        return 'Teaching Math';
-    } 
-    else if (todayClass === 'History') {
-        return 'Teaching History';
-    }
-}
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
